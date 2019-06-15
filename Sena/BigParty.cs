4dee5b53c340ec0a4f7d42502d3b269a5916fc51
JiongSena <51418112+JiongSena@users.noreply.github.com>
2019-06-15 11:13:53 +0800

    Update BigParty.cs

--- Sena/BigParty.cs
+++ Sena/BigParty.cs
@@ -5,19 +5,13 @@ namespace Sena
 {
     public class BigParty : MonoBehaviour
     {
-        List<UnityEngine.GameObject> Locks = new List<GameObject>();
-        private void OnEnable()
-        {
-	Reflection.SetFieldValue<RoR2Application>("maxPlayers", maxPlayers);
-	Reflection.SetFieldValue<RoR2Application>("hardMaxPlayers", hardMaxPlayers 16);
-	Reflection.SetFieldValue<RoR2Application>("maxLocalPlayers", maxLocalPlayers 16);
+            typeof(RoR2.RoR2Application).SetField("maxPlayers", 16);
+            UnityEngine.Networking.NetworkManager.singleton.maxConnections = 16;
         }
         private void OnDisable()
         {
-            typeof(RoR2.RoR2Application).SetField("maxPlayers", 16);
-            typeof(RoR2.RoR2Application).SetField("hardMaxPlayers", 16);
-            typeof(RoR2.RoR2Application).SetField("maxLocalPlayers", 16);
-            UnityEngine.Networking.NetworkManager.singleton.maxConnections = 16;
+            typeof(RoR2.RoR2Application).SetField("maxPlayers", 4);
+            UnityEngine.Networking.NetworkManager.singleton.maxConnections = 4;
         }
     }
 }
